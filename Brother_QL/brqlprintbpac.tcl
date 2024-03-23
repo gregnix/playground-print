53ee293861d7d1d43421c23d7160fab0583091a8
gregnix <77677781+gregnix@users.noreply.github.com>
2024-03-23 13:50:06 +0100

    Add files via upload

--- Brother_QL/brqlprintbpac.tcl
+++ Brother_QL/brqlprintbpac.tcl
@@ -1,10 +1,10 @@
 #!/usr/bin/env tclsh
 
 package require twapi
-# Version 23.03.2024 1250
+# Version 23.03.2024 1340
 # Brother bPAC SDK
 # Make a label with the P-Touch Editor from Brother,
-# create a text field there and save it under ../lbx/test1.lbx
+# create a text field there and save it under ../lbx/company.lbx
 
 
 set dirname [file dirname [info script]]
@@ -18,7 +18,7 @@ catch {file mkdir $logdir}
 
 set template company.lbx
 set label [string map {/ \\} [file join $liblbx $template]]
-set expbmp [string map {/ \\} [file join $exportdir [file rootname $template].bmp]]
+set expbmp [string map {/ \\} [file join $exportdir [file rootname $template]-${::tcl_platform(pointerSize)}.bmp]]
 
 proc writeFile {filename data} {
   set f [open $filename "w"]
@@ -34,7 +34,8 @@ proc brqlPrint {label expbmp {textOne "test "} {count 1} {print 1} } {
   lappend result tcl_platform(pointerSize) $::tcl_platform(pointerSize)
   lappend result tcl_platform(machine)  $::tcl_platform(machine)
   lappend result ::env(PROCESSOR_ARCHITECTURE) $::env(PROCESSOR_ARCHITECTURE)
-    lappend result ::env(SESSIONNAME) $::env(SESSIONNAME)
+  lappend result ::env(SESSIONNAME) $::env(SESSIONNAME)
+  lappend result nameofexecutable [info nameofexecutable]
   lappend result label $label
   lappend result {file exists} [file exists $label]
   lappend result expbmp $expbmp
@@ -76,7 +77,7 @@ proc brqlPrint {label expbmp {textOne "test "} {count 1} {print 1} } {
   return [lappend result prettyresult $prettyresult]
 }
 
-#
+# 
 set count 1
 set print 0
 set result [brqlPrint $label $expbmp "text name\ncompany"  $count $print]
@@ -84,12 +85,14 @@ set result [brqlPrint $label $expbmp "text name\ncompany"  $count $print]
 #Output conole
 puts [dict get $result prettyresult ]
 
-#output a file in ./log/log.txt
+#Output a file in ./log/log-x.txt
+#tcl_platform(pointerSize) 4 -> 32 Bit  8 -> 64 Bit
 writeFile [file join $logdir log-${::tcl_platform(pointerSize)}.txt] [dict get $result prettyresult ]
 
-#Outout text widget
+#Output text widget
 if {[file tail [info nameofexecutable]] eq "wish.exe"} {
   text .t
   .t insert end [dict get $result prettyresult]
   pack .t -expand 1 -fill both
 }
+
